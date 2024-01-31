f828fcd529da81fda64fcbe753390fca1006c55d
Aiman Rosli <aimanrosli977@yahoo.com>
2024-02-01 01:20:22 +0800

    Completed built an SalaryPrediction Application
    Signed-off-by: Aiman Rosli <aimanrosli977@yahoo.com>

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from predict_page import show_predict_page
+from explore_page import show_explore_page
+
+#Create sidebar
+page = st.sidebar.selectbox("Explore Or Predict", ("Predict", "Explore"))
+
+if page == "Predict":
+    #Show the prediction page
+    show_predict_page()
+else:
+    show_explore_page()
